a1e2ca929c9a567d2411c3d7ef90ad54cfe0897c
주환 <69393030+dlwnghks6821@users.noreply.github.com>
2021-01-12 22:41:57 +0900

    c#연습

---
+++ practice.cs
@@ -0,0 +1,44 @@
+using System;
+
+namespace new01
+{
+    class Program
+    {
+        static void Main(string[] args) {
+            A a1 = new A();
+            B b1 = new B();
+            b1.Running();
+
+         }
+        
+          
+        }
+   
+}
+//클래스 A는 부모 , B는 자식 
+class A
+{
+    public int k;
+    public int l;
+    
+   public A()
+    {
+        k = 20;
+        l = 70;
+
+    }
+        
+    
+
+}
+class B : A
+{
+   public void Running()
+    {
+        Console.WriteLine(k + l);
+    }
+}
+
+
+
+
