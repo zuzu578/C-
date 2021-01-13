2b9663675c3c002d8c11a05423e132942b6c8f99
주환 <69393030+dlwnghks6821@users.noreply.github.com>
2021-01-13 10:57:58 +0900

    Add files via upload

---
+++ 구조체.cs
@@ -0,0 +1,31 @@
+using System;
+
+namespace new02
+{
+    class Program
+    {
+        //구조체 사용//
+        struct S{
+            public int k;
+            public int j;
+            public int q;
+
+        }
+        static void Main(string[] args)
+        {
+
+            S s = new S();
+            s.k = 20;
+            s.j = 40;
+            s.q = 50;
+            Draw(s); 
+
+            static void Draw(S s)
+            {
+                Console.WriteLine(s.k + s.j);
+            }
+            
+
+        }
+    }
+}
