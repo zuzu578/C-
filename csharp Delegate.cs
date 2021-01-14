0692d554e52e1b9e65800c07290d29ccf3e7c1cc
주환 <69393030+dlwnghks6821@users.noreply.github.com>
2021-01-14 21:30:09 +0900

    delegate

---
+++ csharp Delegate.cs
@@ -0,0 +1,29 @@
+using System;
+using ExDel;
+
+namespace new03
+{
+    class Program
+    {
+        //delegate
+        static void Main(string[] args)
+        {
+            Del d1 = new Del();
+            SayDelegate say = d1.Hi;
+            say();
+        }
+    }
+}
+
+namespace ExDel
+{
+    public class Del
+    {
+        public void Hi() => Console.WriteLine("Hello world");
+    }
+    //delegate 생성
+    delegate void SayDelegate();
+    
+
+
+}
