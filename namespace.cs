38fde2798e5f597519d03abe8723890f5b87fc05
주환 <69393030+dlwnghks6821@users.noreply.github.com>
2021-01-14 19:22:09 +0900

    네임스페이스 , using

---
+++ namespace.cs
@@ -0,0 +1,52 @@
+using System;
+using System.Collections.Generic;
+
+//namespace ==> java 에서의 package
+//클래스 , 메서드의 이름이 같아도 서로다른 namespace에 소속되어있으면 다른 클래스이다 .
+
+namespace new02
+{
+    public class C
+    {
+        public void Go()
+        {
+            Console.WriteLine("new02's go");
+
+        }
+    }
+    namespace new03
+    {
+        public class C
+        {
+            public void Go()
+            {
+                Console.WriteLine("new03's go");
+            }
+        }
+    }
+    namespace MyPractice
+    {
+        class Program
+        {
+            static void Main(string[] args)
+            {
+             new02.C c1 = new new02.C ();
+                c1.Go();
+                new03.C c2 = new new03.C();
+                c2.Go();
+            }
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
