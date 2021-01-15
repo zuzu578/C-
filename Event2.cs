74751675f6d2225e08f9e245202bc7f6890a4adc
주환 <69393030+dlwnghks6821@users.noreply.github.com>
2021-01-15 14:29:07 +0900

    Add files via upload

---
+++ Event2.cs
@@ -0,0 +1,36 @@
+using System;
+
+namespace EventEx
+{
+    public class ButtonClass
+    {
+        //delegate
+        public delegate void EventHandler();
+        //event
+        public event EventHandler Click;
+        //event handler
+        public void OnClick()
+        {
+            if(Click!= null)
+            {
+                Click();
+            }
+        }
+
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ButtonClass btn = new ButtonClass();
+            btn.Click += Hi1;
+            btn.Click += Hi2;
+            btn.OnClick();
+        }
+        static void Hi1() => Console.WriteLine("Hi1");
+        static void Hi2() => Console.WriteLine("Hi1");
+
+
+    }
+}
